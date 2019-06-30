64609b4d8178dcf1ccec7fa37eadc93931dac7ab
Shivank Garg <shivankgarg98@gmail.com>
2019-06-30 22:22:18 +0100

    add sysctl for rules and parser

--- sys/security/mac_ipacl/mac_ipacl.c
+++ sys/security/mac_ipacl/mac_ipacl.c
@@ -8,6 +8,7 @@
 #include <sys/mutex.h>
 #include <sys/priv.h>
 #include <sys/queue.h>
+#include <sys/socket.h>
 #include <sys/sysctl.h>
 #include <sys/systm.h>
 #include <sys/types.h>
@@ -65,6 +66,7 @@ struct ip_rule {
 	int			jid; /* or name it int pr_id?*/
 	bool			allow; /*allow or deny */
 	struct			ifnet *ifp; /*network interface*/
+	//sa_family_t
 	struct			ipacl_addr addr;
 	struct			ipacl_addr mask;
 	/* currently I am thinking if user gives some special value
@@ -79,7 +81,7 @@ struct ip_rule {
  */
 static struct mtx			rule_mtx;
 static TAILQ_HEAD(rulehead, ip_rule)	rule_head;
-//static char				rule_string[MAC_RULE_STRING_LEN];
+static char				rule_string[MAC_RULE_STRING_LEN];
 
 static void
 toast_rules(struct rulehead *head)
@@ -110,7 +112,7 @@ ipacl_destroy(struct mac_policy_conf *conf)
 
 /*
  * to add rule parser, exact format is yet to decide
- * It can be jid:allow:ifp:ipaddr:mask.
+ * It can be jid@allow@ifp@AF@ipaddr@mask
  * to see if mask can be given in both way(like 255.0.0.0
  * or /8 as user wish
  */
@@ -122,14 +124,15 @@ ipacl_destroy(struct mac_policy_conf *conf)
 static int
 parse_rule_element(char *element, struct ip_rule **rule)
 {
-	char *jid, *allow, *ifp, *ipaddr, *mask, *p;
+	char *jid, *allow, *ifp, *af, *ipaddr, *mask, *p;
 	struct ip_rule *new;
 	int error;
+	int AF;
 
 	error = 0;
 	new = malloc(sizeof(*new), M_IPACL, M_ZERO | M_WAITOK);
 
-	jid = strsep(&element, ":"); /*specifying the jail_name instead
+	jid = strsep(&element, "@"); /*specifying the jail_name instead
 				       of jid will be done later*/
 	if (jid == NULL) {
 		error = EINVAL;
@@ -140,17 +143,13 @@ parse_rule_element(char *element, struct ip_rule **rule)
 		error = EINVAL;
 		goto out;
 	}
-	allow = strsep(&element, ":");
+	allow = strsep(&element, "@");
 	if (allow == NULL) {
 		error = EINVAL;
 		goto out;
 	}
 	new->allow=strtol(allow, &p, 10);
-	ifp = strsep(&element, ":");
-	if (*p != '\0') {
-		error = EINVAL;
-		goto out;
-	}
+	ifp = strsep(&element, "@");
 	if (ifp == NULL) {
 		error = EINVAL;
 		goto out;
@@ -158,23 +157,39 @@ parse_rule_element(char *element, struct ip_rule **rule)
 	/*
 	 * TO_SEE-HOW TO FIND THE INTERFACE FROM ITS NAME
 	 */
-	ipaddr = strsep(&element, ":");
+	af = strsep(&element, "@");
+	if (af == NULL) {
+		error = EINVAL;
+		goto out;
+	}
+
+	AF = (strcmp(af, "AF_INET") == 0) ? AF_INET : 
+             (strcmp(af, "AF_INET6") == 0) ? AF_INET6 : -1;
+	if (AF == -1) {
+		error = EINVAL;
+		goto out;
+	}	
+
+	ipaddr = strsep(&element, "@");
 	if (ipaddr == NULL) {
 		error = EINVAL;
 		goto out;
 	}
-	/*convert string to ip_addr.*/
+	if (inet_pton(AF, ipaddr, new->addr.addr32) != 1) {
+		error = EINVAL;
+		goto out;
+	}
+
+	/*convert string to ip_addr. also to distingish ip4 and ip6*/
 	mask = element;
 	if (mask == NULL) {
 		error = EINVAL;
 		goto out;
 	}
-	/*mask can be given as ip_addr or number*/
-
-
-
-
-
+	if (inet_pton(AF, mask, new->mask.addr32) != 1) {
+		error = EINVAL;
+		goto out;
+	}
 
 out:
 	if (error != 0) {
@@ -185,6 +200,8 @@ out:
 	return (error);
 }
 
+/* Eg:sysctl security.mac.ipacl.rules=1@1@epair0b@AF_INET@192.168.42.2@192.168.0.0 */  
+/* Eg:sysctl security.mac.ipacl.rules=0@0@epair0b@AF_INET6@FE80::0202:B3FF:FE1E:8329@FE80::0202:B3FF:FE1E:8320 */
 static int
 parse_rules(char *string, struct rulehead *head)
 {
@@ -208,6 +225,94 @@ out:
 }
 
 static int
+sysctl_rules(SYSCTL_HANDLER_ARGS)
+{
+	char *string, *copy_string, *new_string;
+	struct rulehead head, save_head;
+	int error;
+
+	new_string = NULL;
+	if (req->newptr != NULL) {
+		new_string = malloc(MAC_RULE_STRING_LEN, M_IPACL,
+		    M_WAITOK | M_ZERO);
+		mtx_lock(&rule_mtx);
+		strcpy(new_string, rule_string);
+		mtx_unlock(&rule_mtx);
+		string = new_string;
+	} else
+		string = rule_string;
+
+	error = sysctl_handle_string(oidp, string, MAC_RULE_STRING_LEN, req);
+	if (error)
+		goto out;
+
+	if (req->newptr != NULL) {
+		copy_string = strdup(string, M_IPACL);
+		TAILQ_INIT(&head);
+		error = parse_rules(copy_string, &head);
+		free(copy_string, M_IPACL);
+		if (error)
+			goto out;
+
+		TAILQ_INIT(&save_head);
+		mtx_lock(&rule_mtx);
+		TAILQ_CONCAT(&save_head, &rule_head, r_entries);
+		TAILQ_CONCAT(&rule_head, &head, r_entries);
+		strcpy(rule_string, string);
+		mtx_unlock(&rule_mtx);
+		toast_rules(&save_head);
+	}
+out:
+	if (new_string != NULL)
+		free(new_string, M_IPACL);
+	return (error);
+}
+
+SYSCTL_PROC(_security_mac_ipacl, OID_AUTO, rules,
+       CTLTYPE_STRING|CTLFLAG_RW, 0, 0, sysctl_rules, "A", "IP ACL Rules");
+
+/*
+ * printing rules for debug
+ */
+static int
+rule_printf(){
+	struct ip_rule *rule;
+	char buf[32];
+	char buf6[128];
+	for (rule = TAILQ_FIRST(&rule_head);
+	    rule != NULL;
+	    rule = TAILQ_NEXT(rule, r_entries)) {
+		/*rough printing of rules*/
+		printf("jid=%d allow=%d",rule->jid, rule->allow);
+		if (inet_ntop(AF_INET, &(rule->addr.v4), buf, sizeof(buf)) != NULL)
+			printf("inet addr: %s\n", buf);
+		if (inet_ntop(AF_INET, &(rule->mask.v4), buf, sizeof(buf)) != NULL)
+			printf("inet addr: %s\n", buf);
+
+		if (inet_ntop(AF_INET6, &(rule->addr.v6), buf6, sizeof(buf6)) != NULL)
+			printf("inet addr: %s\n", buf6);
+		if (inet_ntop(AF_INET6, &(rule->mask.v6), buf6, sizeof(buf6)) != NULL)
+			printf("inet addr: %s\n", buf6);
+
+	}
+
+	return 0; 
+}
+
+static int
+rules_check(struct ucred *cred,
+    const struct in_addr *ia, struct ifnet *ifp)
+{
+	//struct rule *rule;
+	//int error;
+
+/*to distinguish ipv4 rules and ipv6 rules somehow */
+
+	return 0;
+
+}
+
+static int
 ipacl_ip4_check_jail(struct ucred *cred,
     const struct in_addr *ia, struct ifnet *ifp)
 {
@@ -215,8 +320,10 @@ ipacl_ip4_check_jail(struct ucred *cred,
 	if (!ipacl_enabled || !jailed(cred))
 		return 0;
 	
+	//rule_printf();
+	
 	if (ipacl_ipv4)
-		return 0;
+		return rules_check(cred, ia, ifp);
 
 	return (EPERM);
 }
@@ -230,8 +337,8 @@ ipacl_ip6_check_jail(struct ucred *cred,
 		return 0;
 	
 	if (ipacl_ipv6)
+		//return rules_check(cred, ia6, ifp);
 		return 0;
-
 	return (EPERM);
 }
 
