070bc638f68ab6ca10057efa1e5f152353d0ffd0
Shivank Garg <shivankgarg98@gmail.com>
2019-07-14 09:48:15 +0100

    add more IPv4 tests

--- sys/security/mac_ipacl/tests/ip4_test.sh
+++ sys/security/mac_ipacl/tests/ip4_test.sh
@@ -3,7 +3,8 @@
 dir=`dirname $0`
 . ${dir}/ipacl_script.sh
 
-echo "1..20" 
+echo "1..28" 
+jid1=1
 jid2=5
 #run this script for epair0a and epair0b as of now
 #use ifconfig epair create to generate epair
@@ -28,25 +29,37 @@ exec_test ok ipv4 epair0a '127.1.32.31' 24 0
 exec_test fl ipv4 epair0b '198.18.0.12' 15 1
 
 # rule: jid@allow@interface_name@addr_family@ip_addr@subnet_mask
-sysctl security.mac.ipacl.rules=1@1@epair0b@AF_INET@192.168.42.2@-1,${jid2}@1@epair1b@AF_INET@127.1.32.1@-1,${jid2}@1@@AF_INET@198.18.0.1@15,${jid2}@0@@AF_INET@198.18.0.12@-1 >/dev/null
+sysctl security.mac.ipacl.rules=${jid1}@1@epair0b@AF_INET@192.168.42.2@-1,${jid2}@1@epair1b@AF_INET@127.1.32.1@-1,${jid2}@1@@AF_INET@198.18.0.1@15,${jid2}@0@@AF_INET@198.18.0.12@-1 >/dev/null
 
-# Verify if security.mac.ipacl.rule=TODO allow jail to set certain IPv4 address
-exec_test ok ipv4 epair0b '192.168.42.2' 16 1
-exec_test fl ipv4 epair0b '192.168.43.3' 16 1
+# Verify if security.mac.ipacl.rules allow jail to set certain IPv4 address
+exec_test ok ipv4 epair0b '192.168.42.2' 16 ${jid1}
+exec_test fl ipv4 epair0b '192.168.43.3' 16 ${jid1}
 exec_test ok ipv4 epair1b '127.1.32.1' 24 ${jid2}
 exec_test fl ipv4 epair0b '127.1.32.1' 24 ${jid2}
 
-# Verify if scurity.mac.ipacl.rule allow jail to set any address in subnet
+# Verify if scurity.mac.ipacl.rules allow jail to set any address in subnet
 exec_test ok ipv4 epair1b '198.18.0.192' 15 ${jid2}
 exec_test ok ipv4 epair1b '198.18.132.121' 15 ${jid2}
 exec_test fl ipv4 epair1b '197.1.123.123' 15 ${jid2}
 exec_test fl ipv4 epair1b '198.18.0.12' 15 ${jid2} #last rule disllow the ip in that subnet
 
-# Verify if secutiy.mac.ipacl.rule=TODO(interface wildcard) allow jail to set certain IPv4 address
+# Verify if security.mac.ipacl.rules (interface wildcard) allow jail to set certain IPv4 address
 exec_test ok ipv4 lo0 '198.18.0.192' 15 ${jid2}
 exec_test ok ipv4 lo0 '198.18.132.121' 15 ${jid2}
 exec_test fl ipv4 lo0 '197.1.123.123' 15 ${jid2}
 exec_test fl ipv4 lo0 '198.18.0.12' 15 ${jid2} #last rule disllow the ip in that subnet
+
+sysctl security.mac.ipacl.rules=${jid1}@1@@AF_INET@169.254.0.0@16,${jid1}@0@@AF_INET@169.254.123.0@24,${jid1}@1@@AF_INET@169.254.123.123@-1,${jid1}@1@@AF_INET@198.51.100.0@24,${jid1}@0@@AF_INET@198.51.100.100@-1
+# Add more tests from Link-Local space and Documentation(TEST-NET-3)
+exec_test ok ipv4 epair0b '169.254.121.121' 16 ${jid1}
+exec_test fl ipv4 epair0b '169.254.123.121' 16 ${jid1}
+exec_test ok ipv4 epair0b '169.254.123.123' 16 ${jid1}
+exec_test fl ipv4 epair0b '169.253.121.121' 16 ${jid1}
+
+exec_test ok ipv4 lo0 '198.51.100.001' 24 ${jid1}
+exec_test ok ipv4 lo0 '198.51.100.254' 24 ${jid1}
+exec_test fl ipv4 epair0b '198.51.100.100' 24 ${jid1}
+exec_test fl ipv4 epair0b '198.151.100.100' 24 ${jid1}
 
 sysctl security.mac.ipacl.ipv4=0 >/dev/null
 sysctl security.mac.ipacl.rules= >/dev/null
